313cd270474f2d8f95068f3be9618d3053a9efc4
Zshail41 <zshailred41@gmail.com>
2020-05-19 15:49:48 -0300

    Actualizado

---
+++ clase_4/dom.js
@@ -0,0 +1,7 @@
+    var $div = document.querySelector("div")
+
+   // $div.style.height = "100px"
+   // $div.style.width = "100px"
+    // $div.style.backgroundColor = "red"
+
+    $div.classList.add("square")
