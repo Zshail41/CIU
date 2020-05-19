3a8742b07a9fa3f5763ce4f0b346629b1e83adb1
Zshail41 <zshailred41@gmail.com>
2020-05-19 19:21:49 -0300

    Actualizado

---
+++ clase_4/DOM1-8/shuffle.js
@@ -0,0 +1,9 @@
+var divSource = document.getElementById("source").textContent;
+var swap1 = document.getElementById("swap_1").textContent;
+var swap2 = document.getElementById("swap_2").textContent;
+
+
+document.getElementById("destination").textContent = divSource;
+
+document.getElementById("swap_1").textContent = swap2;
+document.getElementById("swap_2").textContent = swap1;
